a7f3c4c7220ae6cf1d68c8c501aff38246107951
Mike Zamansky <zamansky@gmail.com>
2020-01-09 08:01:10 -0500

    got part one of demo going

--- src/main.cljs
+++ src/main.cljs
@@ -3,22 +3,40 @@
             [cljs.core.async :refer (chan put! <! go go-loop timeout)]
             ))
 
+(def counter (r/atom 0))
 
+(def event-queue (chan))
+
+(go-loop [ [event payload] (<! event-queue)]
+  (case event
+    :inc (swap! counter #(+ % payload))
+    :dec (swap! counter #(- % payload))
+    )
+  (recur (<! event-queue))
+  )
 
 (defn main-component []
   [:div 
-   [:h1 "This is a component"]
-   ])
+   [:h1.text-2xl.font-bold "This is a component from cljs"]
+   [:h1.text-4xl.font-bold.p-4 {
+                                :class (if (< @counter 10)
+                                         "bg-green-400"
+                                         "bg-red-400")
+                                :on-click #(put! event-queue [:inc 1])} @counter]
+   
+   (into [:ol.p-4] (for [item (range @counter)] [:li item]))
+   ]
+  )
 
 
 (defn mount [c]
-  (r/render-component [c] (.getElementById js/document "app"))
-  )
+(r/render-component [c] (.getElementById js/document "app"))
+)
 
 (defn reload! []
-  (mount main-component)
-  (print "Hello reload!"))
+(mount main-component)
+(print "Hello reload!"))
 
 (defn main! []
-  (mount main-component)
-  (print "Hello Main"))
+(mount main-component)
+(print "Hello Main"))
